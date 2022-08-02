2306b39563454596ccb0ed852946b2e1cdf6d868
mustafakarakas61 <mustafakarakas61ts862@gmail.com>
2022-08-02 12:59:24 +0300

    First Commit

---
+++ src/main/java/com/mustafa/jdbcOperator/JdbcOperatorApplication.java
@@ -0,0 +1,13 @@
+package com.mustafa.jdbcOperator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcOperatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcOperatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustafa/jdbcOperator/JdbcOperatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafa.jdbcOperator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcOperatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
